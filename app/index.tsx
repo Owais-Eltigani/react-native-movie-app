dcdfc5782469bfe3cad6cc4e2e34197406e8e4ca
Owais-Eltigani <ambidextrous.in@gmail.com>
2025-06-06 19:38:46 +0530

    fixed nativewincss

--- app/index.tsx
+++ app/index.tsx
@@ -2,13 +2,10 @@ import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>hello world</Text>
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black p-4">
+      <Text className=" bg-slate-500 p-5 rounded-lg text-2xl text-slate-100">
+        hello world test
+      </Text>
     </View>
   );
 }
