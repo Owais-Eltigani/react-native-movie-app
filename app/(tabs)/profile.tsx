2231d95ecfa4546e3be6b2c2f8584018834eec79
Owais-Eltigani <ambidextrous.in@gmail.com>
2025-06-09 20:36:42 +0530

    saved & profle features to be implemented later on

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,10 +1,14 @@
+import { icons } from '@/assets/constants/icons';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 const Profile = () => {
   return (
     <View className="bg-primary flex-1">
-      <Text>Profile</Text>
+      <View className="flex-1  justify-center items-center  flex-col gap-4 ">
+        <Image source={icons.person} className="size-10" tintColor={'#eff'} />
+        <Text className="bg-gray-500 text-base">to be implemented</Text>
+      </View>
     </View>
   );
 };
--- app/(tabs)/saved.tsx
+++ app/(tabs)/saved.tsx
@@ -1,10 +1,16 @@
+import { icons } from '@/assets/constants/icons';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
-export default function Saved() {
+const Profile = () => {
   return (
     <View className="bg-primary flex-1">
-      <Text>Saved</Text>
+      <View className="flex-1  justify-center items-center  flex-col gap-4 ">
+        <Image source={icons.save} className="size-10" tintColor={'#eff'} />
+        <Text className="bg-gray-500 text-base">to be implemented</Text>
+      </View>
     </View>
   );
-}
+};
+
+export default Profile;
